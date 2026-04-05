0e56fe24797857efb157c830d20098eaef07b596
Andrew Egan <aeganwork@gmail.com>
2026-04-05 12:56:31 +1000

    Created working Insertion Sort Algorithm!
    Tested insertion sort algorithm with predefined array data, confirmed working as normal. Sorted an array {12,11,13,5,6} -> {5,6,11,12,13}.

--- Headers/InsertionSort.h
+++ Headers/InsertionSort.h
@@ -1,4 +1,12 @@
 #ifndef CXX_INSERTIONSORT_VS_QUICKSORT_COMPARE_INSERTIONSORT_H
 #define CXX_INSERTIONSORT_VS_QUICKSORT_COMPARE_INSERTIONSORT_H
 
+#include <iostream>
+
+using namespace std;
+
+void insertionSort(int arr[], int n);
+
+void printArray(int arr[], int n);
+
 #endif //CXX_INSERTIONSORT_VS_QUICKSORT_COMPARE_INSERTIONSORT_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "Headers/InsertionSort.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int arr[] = {12,11,13,5,6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << endl;
+    printArray(arr, n);
+    cout << endl;
+    insertionSort(arr, n);
+    cout << endl;
+    printArray(arr, n);
+    cout << endl;
+
     return 0;
 }
