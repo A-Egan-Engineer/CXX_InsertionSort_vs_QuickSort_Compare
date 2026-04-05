cde78f2a945b0bb3434a8a3fb36ce3e39a38736c
Andrew Egan <aeganwork@gmail.com>
2026-04-05 14:22:11 +1000

    .txt file read implemented into dynamic array!
    Data is read and passed into dynamic array, then sorted using created InsertionSort function to numerically sort data, and execution time displayed, array deleted from heap!

--- main.cpp
+++ main.cpp
@@ -1,20 +1,53 @@
 #include <iostream>
 #include "Headers/InsertionSort.h"
+#include <fstream>
+#include <chrono>
+#include <unistd.h>
 
 using namespace std;
 
+typedef chrono::high_resolution_clock Clock;
+typedef chrono::milliseconds ms;
+
 int main() {
 
-    int arr[] = {12,11,13,5,6};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    ifstream file("Input Data/a2_task1_input1.txt");
+    if (file.is_open()) {
+        cout << "File opened correctly" << endl;
+    }
+    if (!file.is_open()) {
+        cout << "Error opening file" << endl;
+        return 0;
+    }
+
+    int count;
 
     cout << endl;
-    printArray(arr, n);
+    file >> count;
     cout << endl;
-    insertionSort(arr, n);
+    cout << "Total count of intergers in .txt: " << count << endl;
     cout << endl;
-    printArray(arr, n);
+
+    int* arr1 = new int[count];
+
+    for (int i = 0; i < count; i++) {
+        file >> arr1[i];
+    }
+    file.close();
+
+    for (int i = 0; i < count; i++) {
+        cout << arr1[i] << endl;
+    }
+
     cout << endl;
+
+    auto start1 = Clock::now();
+    insertionSort(arr1, count);
+    auto end1 = Clock::now();
+    auto executionTime1 = chrono::duration_cast<ms>(end1 - start1);
+    cout << "Execution Time: " << executionTime1.count() << " ms" << endl;
+
+    delete [] arr1;
 
     return 0;
 }
